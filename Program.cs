ce9397f3c191255547abb9e122978776919211a7
hbarwick <hallambarwick@hotmail.com>
2022-06-04 19:34:05 +0100

    Add methods for returning sessions from DB. Implement Clock in/out to update the Session.

--- Program.cs
+++ Program.cs
@@ -4,15 +4,6 @@ using CodingTracker;
 
 var db = new DatabaseManager();
 db.CreateDatabase();
-var newSession = new Session();
-
-DateTime start = DateTime.Parse("03/06/2022 09:55:23");
-DateTime end = DateTime.Now;
-
-newSession.StartTime = start;
-newSession.EndTime = end;
-
-Console.WriteLine(newSession.Duration);
 
 var ui = new UIManager(db);
 
